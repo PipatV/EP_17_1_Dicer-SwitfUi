398c20cd1f9eb9991af607f56fac4a3967de3bce
Pack <packaq3@gmail.com>
2022-09-07 16:16:40 +0700

    Initial Commit

---
+++ Dicer-SwitfUi/Dicer_SwitfUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicer_SwitfUiApp.swift
+//  Dicer-SwitfUi
+//
+//  Created by Pipat on 7/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Dicer_SwitfUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
